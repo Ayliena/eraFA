cf51f71973f159179a03a35a4d4eb6ba111e4f3a
Ayliena <tegamdtm@free.fr>
2018-10-26 18:21:45 +0000

    added basic stuff for vetinfo

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
 
-# A very simple Flask Hello World app for you to get started with...
+# ERA FA management flask app
 
-from flask import Flask, render_template, redirect, request, url_for
+# jsonify is for debugging
+from flask import Flask, render_template, redirect, request, url_for,jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import and_
 from flask_login import login_user, LoginManager, UserMixin, logout_user, login_required, current_user
@@ -35,11 +36,60 @@ login_manager.init_app(app)
 
 # --------------- STATIC DATA
 
+# NOTE: it must also be changed in main_page.html and cat_page.html
 TabColor = ["INCONNU", "BEIGE", "BEIGE ET BLANC", "BLANC", "BLUE POINT", "CREME", "ECAILLE DE TORTUE", "GRIS", "GRIS CHARTREUX", "GRIS ET BLANC",
              "NOIR", "NOIR ET BLANC", "NOIR ET SMOKE", "NOIR PLASTRON BLANC", "ROUX", "ROUX ET BLANC", "SEAL POINT", "TABBY BLANC", "TABBY BRUN", "TABBY GRIS",
              "TIGRE", "TIGRE BEIGE", "TIGRE BRUN", "TIGRE CREME", "TIGRE GRIS", "TRICOLORE"]
 TabSex = ["INCONNU", "FEMELLE", "MALE"]
-TabHair = ["COURT", "MI-LONG", "LONG"];
+TabHair = ["COURT", "MI-LONG", "LONG"]
+# static since it changes rarely, NOTE: it must also be changed in cat_page.html
+VETlist = [ [8, "Veto (commentaires)"], [ 6, "AMCB Veterinaires" ], [7, "Clinique Mont. Verte"]  ]
+
+# special FA ids (static)
+FAidAD = 3
+FAidDCD = 5
+
+# --------------- HELPER FUNCTIONS
+
+def vetMapToString(vetmap):
+    str = "-------";
+    if vetmap["visit_pv"]:
+        str[0] = 'V'
+    if vetmap["visit_r1"]:
+        str[1] = '1'
+    if vetmap["visit_r2"]:
+        str[2] = '2'
+    if vetmap["visit_sc"]:
+        str[3] = 'S'
+    if vetmap["visit_id"]:
+        str[4] = 'P'
+    if vetmap["visit_tf"]:
+        str[5] = 'T'
+    if vetmap["visit_gen"]:
+        str[6] = 'X'
+    if vetmap["visit_rr"]:
+        str[7] = 'R'
+    return str
+
+def vetStringToMap(vetstr):
+    vmap = {}
+    vmap["visit_pv"] = (str[0] == 'V')
+    vmap["visit_r1"] = (str[1] == '1')
+    vmap["visit_r2"] = (str[2] == '2')
+    vmap["visit_sc"] = (str[3] == 'S')
+    vmap["visit_id"] = (str[4] == 'P')
+    vmap["visit_tf"] = (str[5] == 'T')
+    vmap["visit_gen"] = (str[6] == 'X')
+    vmap["visit_rr"] = (str[7] == 'R')
+    return vmap
+
+def vetAddStrings(vetstr1, vetstr2):
+    str = vetstr1;
+    for i in range(0,7):
+        if str[i] == '-':
+            str[i] = vetstr2[i];
+
+    return str
 
 # --------------- USER CLASS
 
@@ -47,7 +97,9 @@ TabHair = ["COURT", "MI-LONG", "LONG"];
 # > flask shell
 # > from flask_app import db, User
 # > from werkzeug.security import generate_password_hash
-# newuser = User(username="login", password_hash=generate_password_hash("password"), FAname="FA name - ERA", FAid="FA name - public", FAemail="FA email addr", FAisFA=True, FAisOV=False, FAisADM=False, FAisAD=False)
+# newuser = User(username="login", password_hash=generate_password_hash("password"), FAname="FA name - ERA", FAid="FA name - public", FAemail="FA email addr", FAisFA=True, FAisOV=False, FAisADM=False, FAisAD=False, FAisDCD=False, FAisVET=False)
+# newuserFA = User(username="login", password_hash=generate_password_hash("password"), FAname="FA name - ERA", FAid="FA name - public", FAemail="FA email addr", FAisFA=True, FAisOV=False, FAisADM=False, FAisAD=False, FAisDCD=False, FAisVET=False)
+# newuserVET = User(username="login", password_hash=generate_password_hash("password"), FAname="VET name(long)", FAid="name(short)", FAemail="invalid@invalid", FAisFA=False, FAisOV=False, FAisADM=False, FAisAD=False, FAisDCD=False, FAisVET=True)
 # db.session.add(newuser)
 # db.session.commit()
 
@@ -67,6 +119,7 @@ class User(UserMixin, db.Model):
     FAisADM = db.Column(db.Boolean, nullable=False)
     FAisAD = db.Column(db.Boolean, nullable=False)
     FAisDCD = db.Column(db.Boolean, nullable=False)
+    FAisVET = db.Column(db.Boolean, nullable=False)
 #    cats = db.relationship('Cat', backref='owner_id', lazy='dynamic')
 #    icats = db.relationship('Cat', backref='nextowner_id', lazy='dynamic')
 
@@ -102,7 +155,7 @@ class Cat(db.Model):
     registre = db.Column(db.String(8))
     identif = db.Column(db.String(16))
     description = db.Column(db.String(1024))
-    vetshort = db.Column(db.Integer)
+    vetshort = db.Column(db.String(16))
     adoptable = db.Column(db.Boolean)
     vetvisits = db.relationship('VetInfo', backref='cat', lazy=True)
     events = db.relationship('Event', backref='cat', lazy=True)
@@ -119,7 +172,11 @@ class VetInfo(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     cat_id = db.Column(db.Integer, db.ForeignKey('cats.id'), nullable=False)
-    vtype = db.Column(db.Integer)
+    doneby_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    doneby = db.relationship('User', foreign_keys=doneby_id)
+    vet_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    vet = db.relationship('User', foreign_keys=vet_id)
+    vtype = db.Column(db.String(16))
     vdate = db.Column(db.DateTime, default=datetime.now)
     planned = db.Column(db.Boolean)
     requested = db.Column(db.Boolean)
@@ -182,7 +239,7 @@ def index():
                 return render_template("main_spc_page.html", user=current_user, otheruser=theFA, tabcol=TabColor, tabsex=TabSex, tabhair=TabHair,
                     cats=Cat.query.filter( and_(Cat.owner_id==FAid, Cat.nextowner_id== None) ).all())
 
-            return render_template("main_spc_page.html", user=current_user, otheruser=theFA, tabcol=TabColor, tabsex=TabSex, tabhair=TabHair,
+            return render_template("main_page.html", user=current_user, otheruser=theFA, tabcol=TabColor, tabsex=TabSex, tabhair=TabHair,
                 cats=Cat.query.filter( and_(Cat.owner_id==FAid, Cat.nextowner_id== None) ).all(),
                 ocats=Cat.query.filter( and_(Cat.owner_id==FAid, Cat.nextowner_id != None) ).all(),
                 icats=Cat.query.filter_by(nextowner_id=FAid).all())
@@ -255,9 +312,11 @@ def catpage():
 
     if (cmd == "adm_addcathere" or cmd == "adm_addcatgiveFA" or cmd == "adm_addcatputFA") and current_user.FAisADM:
         # generate the new cat using the form information
+        vetstr = vetMapToString(request.form)
+
         theCat = Cat(registre=request.form["c_registre"], name=request.form["c_name"], sex=request.form["c_sex"],
                     color=request.form["c_color"], longhair=request.form["c_hlen"], identif=request.form["c_identif"],
-                    description=request.form["c_description"], adoptable=(request.form["c_adoptable"]=="1"))
+                    description=request.form["c_description"], vetshort=vetstr, adoptable=(request.form["c_adoptable"]=="1"))
 
         # if for any reason the FA is invalid, then put it here
         if cmd != "adm_addcathere":
@@ -277,7 +336,7 @@ def catpage():
             theCat.owner_id = current_user.id
 
         # generate the event
-        theEvent = Event(cat_id=theCat.id, edate=datetime.now(), etext="rajoute dans le systeme")
+        theEvent = Event(cat_id=theCat.id, edate=datetime.now(), etext="rajoute dans le systeme par {}".format(current_user.FAname))
         db.session.add(theEvent)
 
         db.session.add(theCat)
@@ -291,7 +350,7 @@ def catpage():
         return redirect(url_for('index'))
 
     # check if you can access this
-    if theCat.owner_id != current_user.id and not current_user.FAisADM:
+    if (theCat.owner_id != current_user.id or not current_user.FAisFA) and not current_user.FAisADM:
         return render_template("error_page.html", user=current_user, errormessage="insufficient privileges to access cat data")
         return redirect(url_for('index'))
 
@@ -300,7 +359,9 @@ def catpage():
         return render_template("cat_page.html", user=current_user, cat=theCat, falist=FAlist)
 
     # cat commands
-    if cmd == "fa_modcat" and current_user.FAisFA:
+    if cmd == "fa_modcat" or cmd == "fa_modcatr":
+        # return jsonify(request.form.to_dict())
+
         # update cat information
         theCat.name = request.form["c_name"]
         theCat.sex=request.form["c_sex"]
@@ -309,10 +370,40 @@ def catpage():
         theCat.identif=request.form["c_identif"]
         theCat.description=request.form["c_description"]
         theCat.adoptable=(request.form["c_adoptable"] == "1")
-        # generate the event  IS RECORDING THIS USEFUL?
+
+        # generate the vetinfo record, if any, and the associated event
+        newvisit =  ("visit_pv" in request.form or "visit_r1" in request.form or "visit_r2" in request.form or
+            "visit_sc" in request.form or "visit_id" in request.form or "visit_tf" in request.form or "visit_gen" in  request.form)
+
+        if newvisit:
+            # validate the vet
+            vetId = next((x for x in VETlist if x[0]==int(request.form["visit_vet"])), None)
+
+            if not vetId:
+                return render_template("error_page.html", user=current_user, errormessage="vet id is invalid")
+            else:
+                vetId = vetId[0]
+
+            try:
+                VisitDate = datetime.strptime(request.form["visit_date"], "%d/%m/%y")
+            except ValueError:
+                VisitDate = datetime.now()
+
+            VisitType = ""
+
+            theVisit = VetInfo(cat_id=theCat.id, doneby_id=current_user.id, vet_id=vetId, vtype=VisitType, vdate=VisitDate,
+                planned=(request.form["visit_state"] == 1), comments=request.form["visit_comments"])
+            db.session.add(theVisit)
+
+        # TODO
 #        theEvent = Event(cat_id=theCat.id, edate=datetime.now(), etext="rajoute dans le systeme")
 #        db.session.add(theEvent)
+
         db.session.commit()
+
+        # if we stay on the page, regenerate it directly
+        if cmd == "fa_modcatr":
+            return render_template("cat_page.html", user=current_user, cat=theCat, falist=FAlist)
 
         return redirect(url_for('index'))
 
