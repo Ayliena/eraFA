567e1a489f2bb1419b2a2440d55a9bb50a5b4fbf
Ayliena <tegamdtm@free.fr>
2019-06-27 21:15:06 +0000

    added support for bon veterinaire

--- flask_app.py
+++ flask_app.py
@@ -12,7 +12,10 @@ from werkzeug.utils import secure_filename
 import secrets
 from datetime import datetime, timedelta
 import os
+import hashlib
+import base64
 from PIL import Image
+from flask_qrcode import QRcode
 
 UPLOAD_FOLDER = '/home/ishark/eraFA/static'
 ALLOWED_EXTENSIONS = set(['jpg'])
@@ -32,6 +35,8 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+qrcode = QRcode(app)
 
 # database creation:
 # > ipython3.7
@@ -115,6 +120,13 @@ def vetAddStrings(vetstr1, vetstr2):
             str[i] = vetstr2[i]
 
     return "".join(str)
+
+def ERAsum(str1):
+    m = hashlib.md5()
+    m.update((str1 + '0C1s5qzQo5').encode('utf-8'))
+    str2 = base64.b64encode(m.digest()).decode('utf-8')
+
+    return str2[0:12]
 
 #
 # --------------- DATABASE INITIALIZATION (required special FAs)
@@ -302,7 +314,9 @@ def miscpage(cat_id=-1):
     if request.method == "POST":
         return render_template("misc_page.html", user=current_user, FAids=FAidSpecial, msg="POST REQUEST, id={} cmd={}".format(request.form["id"], request.form["action"]))
 
-    return render_template("misc_page.html", user=current_user, FAids=FAidSpecial, msg="GET REQUEST: id={}".format(cat_id))
+#    return render_template("misc_page.html", user=current_user, FAids=FAidSpecial, msg="GET REQUEST: id={}".format(cat_id))
+    catlist = Cat.query.filter_by(name='FILOU').all()
+    return render_template("bonveto_page.html", user=current_user, FAids=FAidSpecial, cats=catlist, fa=current_user, bdate=datetime.now(), vtype=(1,2,3,4,5,6), comments=["1 echographie", "1 autre echographie"])
 
 # --------------- WEB PAGES
 
@@ -493,7 +507,9 @@ def catpage(catid=-1):
 
     elif request.method == "POST":
         cmd = request.form["action"]
-        catid = request.form["catid"]
+
+        if "catid" in request.form and request.form["catid"] != 'None':
+            catid = int(request.form["catid"])
 
     if cmd == "fa_return":
         return redirect(url_for('fapage'))
@@ -746,7 +762,7 @@ def catpage(catid=-1):
         # extract all planned visits which are now executed
         modvisits = []
         for k in request.form.keys():
-            if k.startswith("oldv_") and k.endswith("_state") and int(request.form[k]) != 1:
+            if k.startswith("oldv_") and k.endswith("_state"): # and int(request.form[k]) != 1:
                 modvisits.append(k)
 
         for mv in modvisits:
@@ -769,6 +785,7 @@ def catpage(catid=-1):
                 theEvent = Event(cat_id=theCat.id, edate=datetime.now(), etext="{}: visite vétérinaire {} annullée".format(current_user.FAname, theVisit.vtype))
                 db.session.add(theEvent)
                 db.session.delete(theVisit)
+                updated = True
                 continue
 
             # modify the visit with the new data (we'll need to get all of it....)
@@ -783,16 +800,18 @@ def catpage(catid=-1):
                 continue
 
             # update the record
-            theVisit.vtype = VisitType
+            if theVisit.vtype != VisitType:
+                theVisit.vtype = VisitType
+                updated = True
 
             try:
                 VisitDate = datetime.strptime(request.form[prefix+"_date"], "%d/%m/%y")
             except ValueError:
                 VisitDate = datetime.now()
-            theVisit.vdate = VisitDate
 
-            theVisit.planned = False
-            updated = True
+            if theVisit.vdate != VisitDate:
+                theVisit.vdate = VisitDate
+                updated = True
 
             # validate the vet
             vet = next((x for x in VETlist if x.id==int(request.form[prefix+"_vet"])), None)
@@ -800,14 +819,26 @@ def catpage(catid=-1):
             if not vet:
                 return render_template("error_page.html", user=current_user, errormessage="vet id is invalid", FAids=FAidSpecial)
             else:
-                vetId = vet.id
+                if theVisit.vet_id != vet.id:
+                    theVisit.vet_id = vet.id
+                    updated = True
 
-            theVisit.comments = request.form[prefix+"_comments"]
+            if theVisit.comments != request.form[prefix+"_comments"]:
+                theVisit.comments = request.form[prefix+"_comments"]
+                updated = True
 
-            theCat.vetshort = vetAddStrings(theCat.vetshort, VisitType)
+            if int(request.form[prefix+"_state"]) != 1:
+                # means it's not planned anymore
+                theCat.vetshort = vetAddStrings(theCat.vetshort, VisitType)
+                theVisit.planned = False
+                updated = True
+                et = "effectuee le"
+            else:
+                et = "re-planifiee pour le"
 
-            theEvent = Event(cat_id=theCat.id, edate=datetime.now(), etext="{}: visite vétérinaire {} effectuée le {} chez {}".format(current_user.FAname, VisitType, VisitDate.strftime("%d/%m/%y"), theVisit.vet.FAname))
-            db.session.add(theEvent)
+            if updated:
+                theEvent = Event(cat_id=theCat.id, edate=datetime.now(), etext="{}: visite vétérinaire {} {} {} chez {}".format(current_user.FAname, VisitType, et, VisitDate.strftime("%d/%m/%y"), theVisit.vet.FAname))
+                db.session.add(theEvent)
 
         # end for mv in modvisits
 
@@ -1366,11 +1397,38 @@ def vetpage():
         session["otherMode"] = "special-vetplan"
         return redirect(url_for('fapage'))
 
-    if cmd == "fa_vetmv":
-        return render_template("error_page.html", user=current_user, errormessage="command error (/vet:fa_vetmv)", FAids=FAidSpecial)
+    if cmd == "fa_vetmv" or cmd == "fa_vetmvd":
+        # iterate on the checkboxes to see which cats are to be processed
+        for key in request.form.keys():
+            if key[0:3] == 're_':
+                vvid = int(key[3:])
 
-    if cmd == "fa_vetmvd":
-        return render_template("error_page.html", user=current_user, errormessage="command error (/vet:fa_vetmvd)", FAids=FAidSpecial)
+                # make sure you can access this (= must be one of your cats)
+                theVisit = VetInfo.query.filter_by(id=vvid).first()
+                if not theVisit:
+                    return render_template("error_page.html", user=current_user, errormessage="/vet:fa_vetmv(d): invalid vetinfo id", FAids=FAidSpecial)
+
+                # find the cat and make sure we can access it
+                theCat = Cat.query.filter_by(id=theVisit.cat_id).first()
+                if not (theCat.owner_id == current_user.id and current_user.FAisFA) and not (theCat.owner.FAresp_id == current_user.id) and not current_user.FAisADM:
+                    return render_template("error_page.html", user=current_user, errormessage="/vet:fa_vetmv(d): insufficient privileges", FAids=FAidSpecial)
+
+                # convert the visit to "effectuee" and log the event
+                theCat.vetshort = vetAddStrings(theCat.vetshort, theVisit.vtype)
+                theVisit.planned = False
+
+                if cmd == "fa_vetmvd":
+                    try:
+                        theVisit.vdate = datetime.strptime(request.form["c_vdate"], "%d/%m/%y")
+                    except ValueError:
+                        theVisit.vdate = datetime.now()
+
+                theEvent = Event(cat_id=theCat.id, edate=datetime.now(), etext="{}: visite vétérinaire {} effectuée le {} chez {}".format(current_user.FAname, theVisit.vtype, theVisit.vdate.strftime("%d/%m/%y"), theVisit.vet.FAname))
+                db.session.add(theEvent)
+                db.session.commit()
+
+        # return to the same page
+        return redirect(url_for('fapage'))
 
     if cmd == "fa_vetmpl":
         FAid = current_user.id
@@ -1399,7 +1457,134 @@ def vetpage():
 
 
     if cmd == "fa_vetmpl_save":
-        return render_template("error_page.html", user=current_user, errormessage="command error (/vet:fa_vetmpl_save)", FAids=FAidSpecial)
+        # vet list will be needed
+        VETlist = User.query.filter_by(FAisVET=True).all()
+
+        # start by decoding the visit data
+        # generate the vetinfo record, if any, and the associated event
+        VisitType = vetMapToString(request.form, "visit")
+
+        if VisitType != "--------":
+            # validate the vet
+            vet = next((x for x in VETlist if x.id==int(request.form["visit_vet"])), None)
+
+            if not vet:
+                return render_template("error_page.html", user=current_user, errormessage="vet id is invalid", FAids=FAidSpecial)
+            else:
+                vetId = vet.id
+
+            try:
+                VisitDate = datetime.strptime(request.form["visit_date"], "%d/%m/%y")
+            except ValueError:
+                VisitDate = datetime.now()
+
+            VisitPlanned = (int(request.form["visit_state"]) == 1)
+
+            # if executed, then cumulate with the global
+            if not VisitPlanned:
+                et = "effectuee le"
+            else:
+                et = "planifiee pour le"
+
+        # now iterate on all cats and add the visit
+        for key in request.form.keys():
+            if key[0:3] == 're_':
+                catid = int(key[3:])
+
+                # find the cat and make sure we can access it
+                theCat = Cat.query.filter_by(id=catid).first()
+
+                if not theCat:
+                    return render_template("error_page.html", user=current_user, errormessage="fa_vetmpl_save: invalid cat id {}".format(catid), FAids=FAidSpecial)
+
+                if not (theCat.owner_id == current_user.id and current_user.FAisFA) and not (theCat.owner.FAresp_id == current_user.id) and not current_user.FAisADM:
+                    return render_template("error_page.html", user=current_user, errormessage="/vet:fa_vetmv(d): insufficient privileges", FAids=FAidSpecial)
+
+                # generate the visit and the event
+                # we always associate the visit to the current owner
+                theVisit = VetInfo(cat_id=theCat.id, doneby_id=theCat.owner_id, vet_id=vetId, vtype=VisitType, vdate=VisitDate,
+                    planned=VisitPlanned, comments=request.form["visit_comments"])
+                db.session.add(theVisit)
+                db.session.commit()  # needed for vet.FAname
+
+                # add it as event (planned or not)
+                theEvent = Event(cat_id=theCat.id, edate=datetime.now(), etext="{}: visite vétérinaire {} {} {} chez {}".format(current_user.FAname, VisitType, et, VisitDate.strftime("%d/%m/%y"), theVisit.vet.FAname))
+                db.session.add(theEvent)
+                db.session.commit()
+
+        # return to vet page
+        return redirect(url_for('fapage'))
+
+    if cmd == "fa_vetbon":
+        # generate the data for the bon
+        catlist = []
+        catregs = []
+        theFA = None
+        # vaccinations, rappels, sterilisations, castrations, identifications, tests fiv/felv, soins
+        # if vtype is "soins" then we append the comment as visit description
+        vtypes = [0, 0, 0, 0, 0, 0, 0]
+        vdate = None
+        comments = []
+        # the QR code contains: ERA;<who authorized>;<today's date>;<FAid>;<visit date>;<cat regs>;<vtypes joined as string>;<check>
+        # check is a 12-byte string obtained by md5sum of previous part + some random junk + base64_encode + cut in half
+
+        # iterate on the checkboxes to see which cats are to be processed
+        for key in request.form.keys():
+            if key[0:3] == 're_':
+                vvid = int(key[3:])
+
+                # make sure you can access this (= must be one of your cats)
+                theVisit = VetInfo.query.filter_by(id=vvid).first()
+                if not theVisit:
+                    return render_template("error_page.html", user=current_user, errormessage="/vet:fa_vetmv(d): invalid vetinfo id", FAids=FAidSpecial)
+
+                if not vdate:
+                    vdate = theVisit.vdate
+
+                # find the cat and make sure we can access it
+                theCat = Cat.query.filter_by(id=theVisit.cat_id).first()
+                if not (theCat.owner_id == current_user.id and current_user.FAisFA) and not (theCat.owner.FAresp_id == current_user.id) and not current_user.FAisADM:
+                    return render_template("error_page.html", user=current_user, errormessage="/vet:fa_vetmv(d): insufficient privileges", FAids=FAidSpecial)
+
+                if not theFA:
+                    theFA = theCat.owner
+
+                # cumulate the information
+                catlist.append(theCat)
+                catregs.append(theCat.regStr())
+
+                if theVisit.vtype[0] != '-':
+                    vtypes[0] += 1
+                if theVisit.vtype[1] != '-':
+                    vtypes[1] += 1
+                if theVisit.vtype[2] != '-':
+                    vtypes[1] += 1
+                if theVisit.vtype[3] != '-':
+                    if theCat.sex == 2:
+                        vtypes[3] += 1
+                    else:
+                        vtypes[2] += 1
+                if theVisit.vtype[4] != '-':
+                    vtypes[4] += 1
+                if theVisit.vtype[5] != '-':
+                    vtypes[5] += 1
+                if theVisit.vtype[6] != '-':
+                    comments.append(theVisit.comments)
+                    vtypes[6] += 1
+                if theVisit.vtype[7] != '-':
+                    vtypes[1] += 1
+
+        # if nothing was selected, stay here
+        if not catlist:
+            return redirect(url_for('fapage'))
+
+        bdate = datetime.today()
+
+        # generate the qrcode string
+        qrstr = "ERA;{};{};{};{};{};{};".format(current_user.FAid, bdate.strftime('%Y%m%d'), theFA.FAid, vdate.strftime('%Y%m%d'), "/".join(catregs), "".join(str(e) for e in vtypes))
+        qrstr = qrstr + ERAsum(qrstr)
+
+        return render_template("bonveto_page.html", user=current_user, FAids=FAidSpecial, tabcol=TabColor, tabsex=TabSex, tabhair=TabHair, cats=catlist, fa=theFA, bdate=vdate, vtype=vtypes, comments=comments, qrdata=qrstr)
 
     return render_template("error_page.html", user=current_user, errormessage="command error (/vet)", FAids=FAidSpecial)
 
